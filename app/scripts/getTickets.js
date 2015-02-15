3a18fce1f2d22001ff147a59f6a5d0b46f0fc935
Chris Boette <cboette@gmail.com>
2015-02-15 17:32:12 -0600

    Move disparate functions into groups of functions to call.

--- app/scripts/getTickets.js
+++ app/scripts/getTickets.js
@@ -1,34 +1,36 @@
 /* global SW:true */  
 
+var getUnassigned = function (assignmentCount) {
+  var ticketTotal = 0;
+  for (var property in assignmentCount) {
+    ticketTotal += assignmentCount[property];
+  }
+  var unassignedTickets = data.tickets.length - ticketTotal;
+  console.log('unassignedTickets');
+  return unassignedTickets;
+}
+
+var countAssignments = function(ticket){
+  if (assignmentCount[ticket.assignee.id]){
+    assignmentCount[ticket.assignee.id] += 1;
+  } else {
+    assignmentCount[ticket.assignee.id] = 1;
+  }
+};
+
 $(document).ready(function(){
   'use strict';
   console.log( 'Doing SW things!' );
   var card = new SW.Card();
   var helpdesk = card.services('helpdesk');
   var assignmentCount = {};
-  helpdesk
-    .request('tickets')
+  helpdesk.request('tickets')
     .then( function(data){
-      console.log( 'got data!' );
       $.each(data.tickets, function(index, ticket){
-        console.log( ticket.assignee.id );
-        if (assignmentCount[ticket.assignee.id]){
-          assignmentCount[ticket.assignee.id] += 1;
-        } else {
-          assignmentCount[ticket.assignee.id] = 1;
-        }
-        console.log( 'assignmentCount object' );
-        console.log( assignmentCount );
+        countAssignments(ticket);
       });
-
-      console.log( assignmentCount + 'final' );
-      var ticketTotal = 0;
-      for (var property in assignmentCount) {
-          ticketTotal += assignmentCount[property];
-      }
-      var unassignedTickets = data.tickets.length - ticketTotal;
-      console.log('unassignedTickets');
-      console.log(unassignedTickets);
+    console.log( assignmentCount + 'final' );
+    getUnassigned(assignmentCount);
     });
 
 });
