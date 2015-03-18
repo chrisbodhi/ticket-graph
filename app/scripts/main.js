e2a05c99e7fd0ecd54a67a581ee57a4be5a4464c
Chris Boette <cboette@gmail.com>
2015-03-17 23:21:41 -0500

    Call main.js from the index file. Fold in the horizontal bar graph code into main.js.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,11 +53,13 @@ callSW().then(function(allTickets){
   };
   assigneeIds = uniqIds(assigneeIds);
 
+  // Calculate the days a ticket has been open
   var calcDaysOpen = function(time){
     var floatDays = (Date.now() - new Date(time))/(1000*60*60*24);
     return Math.floor(floatDays);
   };
 
+  // Pull out relevant bits of the ticket in question
   var getTicketData = function(t){
     var info = {};
     info.ticketId = t.id;
@@ -67,6 +69,7 @@ callSW().then(function(allTickets){
     return info;
   };
 
+  // Used for splitting out each assignee's tickets...
   var theirTickets = function(assigneeId){
     return ticketsToChart.filter(function(t, index, arr){
       if (assigneeId === t.assignee.id){
@@ -74,7 +77,7 @@ callSW().then(function(allTickets){
       } 
     });
   };
-
+  // ...done here.
   assigneeIds.forEach(function(a){
     var bucket = { 
                   'assigneeId': a,
@@ -89,66 +92,172 @@ callSW().then(function(allTickets){
     bucket = {};
   });
 
-  var graphData = [];
-  var width = 500,
-      height = 500;
+  // Prioritizing the tickets
+  var priorities = []; // PRIORITIES!
 
-  graphData = dataArray.map(function(e,i,arr){
-    return e.tickets.length;
+  priorities.push(
+    { priority: 'Low', 
+      data: []
+    },
+    { priority: 'Medium', 
+      data: []
+    },
+    { priority: 'High', 
+      data: []}
+    );
+
+  var getCount = function (data, priorityInt) {
+    return data.tickets.filter( function(t,index,arr) {
+      if ( t.priority === priorityInt ){ return 1; }
+    }).length;
+  };
+
+  var assignPriorities = function(d, lowCount, medCount, highCount){
+    priorities.forEach(function(p){
+      if (p.priority === 'Low'){
+        p.data.push({'assId': d.assigneeId, 'count': lowCount });
+      } else if (p.priority === 'Medium'){
+        p.data.push({'assId': d.assigneeId, 'count': medCount });
+      } else if (p.priority === 'High'){
+        p.data.push({'assId': d.assigneeId, 'count': highCount });
+      } else {
+        console.error('No priority noted for ticket ' + d);
+      }
+    });
+  };
+
+  // Prepare the data for processing by D3, here...
+  dataArray.forEach(function(d){
+    var lowCount = getCount(d, 1),
+        medCount = getCount(d, 2),
+        highCount = getCount(d, 3);
+    
+    assignPriorities(d, lowCount, medCount, highCount);
   });
 
-  var widthScale = d3.scale.linear()
-                    .domain([0, Math.max.apply(0, graphData)])
-                    .range([0, width]);
+  console.log(priorities);
+  
+  // ...and here
+  var stackData = priorities.map(function (d){
+    return d.data.map(function (t, i){
+      return {
+        y: t.count,
+        x: t.assId
+      };
+    });
+  });
 
-  var heightScale = (graphData.length * 100);
+  var priorityLevels = priorities.map(function (t){
+    return t.priority;
+  });
 
-  // Suitable for gradient based on age?
-  // var color = d3.scale.linear()
-  //               .domain([0, Math.max.apply(0, graphData)])
-  //               .range(['lightblue', 'dodgerblue']);
+  ///////////////////////////////////////////////////////////////////////////////
+  // GRAPH MAKING
+  var width = 600,
+      height = 500;
 
-  var axis = d3.svg.axis()
-                .ticks(5) // total number of ticks
-                .scale(widthScale);
+
+  var stack = d3.layout.stack();
+
+  stack(stackData);
+
+  stackData = stackData.map(function (group) {
+    return group.map(function (d) {
+      // Invert the x and y values, and y0 becomes x0
+      return {
+        x: d.y,
+        y: d.x,
+        x0: d.y0
+      };
+    });
+  });
+
+  //Set up scales
+  var xMax = d3.max(stackData, function (group) {
+      return d3.max(group, function (d) {
+        return d.x + d.x0;
+      });
+  });
+
+  var xScale = d3.scale.linear()
+          .domain([0, xMax])
+          .range([0, width]);
+
+  // todo: programatically get names into an array
+  var names = [29, 3, 2];        
+  // var names = ['Alice', 'Bob', 'Carol'];        
+
+  var yScale = d3.scale.ordinal()
+          .domain(names)
+          .rangeRoundBands([0, height], 0.1);
+          
+  var colors = d3.scale.category10();
 
   var canvas = d3.select('.jumbotron')
                   .append('svg')
-                  .attr('width', width)
-                  .attr('height', Math.max(height, heightScale))
+                  .attr('width', width + 60)
+                  .attr('height', height)
                   .append('g') //group the rects on the canvas
                   .attr('transform', 'translate(20, 0)');
 
-  var bars = canvas.selectAll('rect')
-                   .data(graphData)
-                   .enter()
-                   .append('rect')
-                   .attr('width', function (d) { return widthScale(d); })
-                   .attr('height', 50)
-                   .attr('fill', 'dodgerblue')
-                   .attr('y', function (d,i) { return i * 100; });
+  // Add a group for each row of data
+  var groups = canvas.selectAll('g')
+                  .data(stackData)
+                  .enter()
+                  .append('g')
+                  .style('fill', function(d, i) {
+                    return colors(i);
+                  });
 
-  // Rather than making the canvas variable more clunky, separate concerns!
+  var rects = groups.selectAll('rect')
+          .data(function(d) { return d; })
+          .enter()
+          .append('rect')
+          .attr('x', function(d) {
+                  return xScale(d.x0);
+          })
+          .attr('y', function(d) {
+                  return yScale(d.y);
+          })
+          .attr('height', function (d) {
+            return yScale.rangeBand();
+          })
+          .attr('width', function(d) {
+                  return xScale(d.x); 
+          })
+          .on('mouseover', function (d) {
+            var xPos = parseFloat(d3.select(this).attr('x')) + width;
+            var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand();
+            
+            d3.select('#d3-tooltip')
+                .style('left', (xPos - 25) + 'px')
+                .style('top', yPos + 'px')
+                .select('#value')
+                .text(d.x);
+
+            d3.select('#d3-tooltip').classed('hidden', false);
+        })
+        .on('mouseout', function () {
+          d3.select('#d3-tooltip').classed('hidden', true);
+        });
+
+  // Axis, no Allies
+  var xAxis = d3.svg.axis()
+      .scale(xScale)
+      .ticks(stackData[0].length)
+      .orient('bottom');
+
+  var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient('left');  
+
   canvas.append('g')
-        .attr('transform', 'translate(0, ' + (heightScale - 20) + ')')
-        .call(axis);
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + 5 + ',' + (height - 20) + ')')
+      .call(xAxis);
 
+  canvas.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + 5 + ', 0)')
+      .call(yAxis); 
 });
-// END TICKET FILTERING AKA GIANT PROMISE RESOLUTION
-///////////////////////////////////////////////////////////////////////////////
-
-
-
-// Reference on tooltips
-  // var tooltip = d3.select('#chart')
-  //                 .append('div')
-  //                 .attr('class', 'd3-tooltip');
-
-  // tooltip.append('div')
-  //        .attr('class', 'label');
-
-  // tooltip.append('div')
-  //        .attr('class', 'count');
-
-  // tooltip.append('div')
-  //        .attr('class', 'percent');
