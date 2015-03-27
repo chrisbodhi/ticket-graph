21b2ba1084ea1bafeb23f24ce359729b5aa6c70d
Chris Boette <cboette@gmail.com>
2015-03-27 19:28:29 -0400

    Clean up unused code. Add tooltips for unassigned bars.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,28 +3,14 @@
 /* global SW:true */
 'use strict';
 
+var ticketData  = [],
+    counts      = [],
+    lowCount    = 0,
+    medCount    = 0,
+    highCount   = 0;
+
 ///////////////////////////////////////////////////////////////////////////////
 // START TICKET FILTERING
-var assigneeIds     = [],
-    dataArray       = [], // this will be passed to D3
-    ticketsToChart  = [];
-
-// var callSW = function(){
-//   var promise = new RSVP.Promise(function(resolve, reject){
-//     var card = new SW.Card();
-//     var helpdesk = card.services('helpdesk');
-//     helpdesk.request('tickets')
-//       .then(function(data) {
-//         if (data.tickets.length > 0){
-//           resolve(data.tickets);
-//         } else {
-//           reject('err');
-//         }
-//       });
-//     });
-//   return promise;
-// };
-
 var getOpenTickets = function(){
   var promise = new RSVP.Promise(function(resolve, reject){
     var card = new SW.Card();
@@ -48,12 +34,6 @@ var getUnassigned = function(openTickets){
   });
   return promise;
 };
-
-var ticketData  = [],
-    lowCount     = 0,
-    medCount     = 0,
-    highCount    = 0,
-    counts       = [];
 
 // Calculate the days a ticket has been open
 var calcDaysOpen = function(time){
@@ -95,20 +75,24 @@ var countTickets = function(ticketData){
       } else if (t.priority === 3){
         lowCount += 1;
       } else {
-        console.log('No priority found.');
+        console.error('No priority found.');
       }
     });
     counts.push(highCount);
     counts.push(medCount);
     counts.push(lowCount);
     if (counts.length > 0){
-      console.log(counts);
       resolve(counts);
     } else {
       reject('err');
     }
   });
   return promise;
+};
+
+var addLabel = function(i){
+  var priorities = {0: "High", 1: "Medium", 2: "Low"};
+  return priorities[i];
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -118,9 +102,6 @@ var graphMaker = function(){
   var width = 500;
   var height = 100;
   var barPadding = 10;
-
-  var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-          11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
   //Create SVG element
   var svg = d3.select(".jumbotron")
@@ -143,17 +124,18 @@ var graphMaker = function(){
         return d * 20;
      })
      .attr("fill", function(d, i) {
-      console.log(1 - (i * 0.4));
       return "rgba(255, 102, 0, " + (1 - (i * 0.4)) + ")";
      })
-     .on('mouseover', function (d) {
+     .on('mouseover', function (d, i) {
        var xPos = parseFloat(d3.select(this).attr('x'));
        var yPos = parseFloat(d3.select(this).attr('y'));
        d3.select('#d3-tooltip')
            .style('left', (xPos + (width * 1.5)) + 'px')
            .style('top', (yPos + (height)) + 'px')
            .select('#value')
-           .text(d);
+           .text(function() {
+              return addLabel(i) + ': ' + d;
+            });
        d3.select('#d3-tooltip').classed('hidden', false);
      })
      .on('mouseout', function () {
@@ -175,7 +157,7 @@ var graphMaker = function(){
         return height - (d * 20) + 14;
      })
      .attr("font-family", "sans-serif")
-     .attr("font-size", "11px")
+     .attr("font-size", "13px")
      .attr("fill", "white");
 };
 
